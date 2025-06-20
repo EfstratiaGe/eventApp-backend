f0ca2c69c5b2ba1946ada34642c091912cf90c3e
EfstratiaGe <xgeragoteli@stuba.sk>
2025-06-20 10:02:05 +0200

    Favorite implementation in Recom Events

--- routes/recoms.js
+++ routes/recoms.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router  = express.Router();
 const Event   = require('../models/event');
+const Favorite = require('../models/favorite');
 
 const DUMMY_USER_ID = 'guest123'; 
 
